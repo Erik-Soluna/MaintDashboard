1e73aa4262d1ea64c4eb44ec34c617e957fdffc3
Erik Wood <erik@example.com>
2025-11-17 14:59:30 -0600

    [REMOVE] Fix migration dependency for PlaywrightDebugLog removal

--- core/migrations/0022_remove_playwrightdebuglog.py
+++ core/migrations/0022_remove_playwrightdebuglog.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0021_brandingsettings_table_hover_background_color_and_more'),
+        ('core', '0020_brandingsettings_table_hover_background_color_and_more'),
     ]
     
     # Note: This migration removes the PlaywrightDebugLog model
